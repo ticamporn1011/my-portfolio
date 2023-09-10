e5dc4ee75c98ba4d45dc94c04ff51fd72947c9c1
ticamporn1011 <ticamporn.g@gmail.com>
2023-09-10 12:32:44 +0700

    update path ver3

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,16 +1,19 @@
 import { useRouteError } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div id="error-page" className='text-center p-5'>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        {/* <i>{error.statusText || error.message}</i> */}
-      </p>
+      <Link to="/">
+        <button type="button" className="btn btn-primary">
+          Home
+        </button>
+      </Link>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,19 @@ import App from './App.tsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage.tsx';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
-  },
-  {
-    path: '*',
-    element: <Navigate to="/" />
+    element: <App />,
+    errorElement: <ErrorPage />
   }
+  // {
+  //   path: '*',
+  //   element: <Navigate to="/" />
+  // }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
